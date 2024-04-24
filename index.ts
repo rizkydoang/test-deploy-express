81279b1278b3eba731d4b34374f3f3e7bd13e69c
rizkydoang <mriski889@gmail.com>
2024-04-24 19:26:29 +0700

    init project

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const PORT = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!')
+    // res.render("../home.ejs", { message: "existsMessage" });
+});
+
+app.listen(PORT, () => {
+    console.log("Server is Successfully Running, and App is listening on port " + PORT)
+});
+
+module.exports = app;
